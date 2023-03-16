6c2cbd657c85bb85562bddd7cfbb6f8cd5c72314
admin <admin@DESKTOP-NVM0BRC>
2023-03-16 12:15:26 +0530

    daynine

---
+++ first_project/src/daynine/VariableDemo.java
@@ -0,0 +1,25 @@
+package daynine;
+
+public class VariableDemo {
+	
+	int s=48;
+	
+	static int g=25;
+	
+	public static void main(String[] args) {
+		int r= 22;
+		
+	    System.out.println(r);
+	    
+	    System.out.println(g);
+	    VariableDemo obj = new VariableDemo();
+	    System.out.println(obj.s);
+	    
+		
+		
+	}
+
+	
+}
+
+
